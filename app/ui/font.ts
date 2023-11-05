3f14a7837ba05b6d20742c74176b9065945fc38f
Sean Coding <104483622+lashawty@users.noreply.github.com>
2023-11-05 18:50:26 +0800

    init

---
+++ app/ui/font.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana, Quicksand } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
+export const quicksand = Quicksand({
+    weight: ['400', '500', '700'],
+    subsets: ['latin']
+})
